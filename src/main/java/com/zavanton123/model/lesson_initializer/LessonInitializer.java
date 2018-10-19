0de53c66beaec3115c175b9d4b755e4849c1aa61
zavanton123 <zavanton123@gmail.com>
2018-10-19 23:09:09 +0300

    Noise file is written to WIP folder.

--- src/main/java/com/zavanton123/model/lesson_initializer/LessonInitializer.java
+++ src/main/java/com/zavanton123/model/lesson_initializer/LessonInitializer.java
@@ -1,14 +1,18 @@
 package com.zavanton123.model.lesson_initializer;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class LessonInitializer {
 
     public void setupLesson(File lessonFolder) {
 
         File wip = createWipFolder(lessonFolder, "WIP");
-
-
+        File noiseFile = createNoiseFile(wip,
+                "noise_5_min.wav",
+                "noise.wav");
 
     }
 
@@ -19,5 +23,35 @@ public class LessonInitializer {
             wip.mkdirs();
         }
         return wip;
+    }
+
+    private File createNoiseFile(File wip, String resourceName, String fileName) {
+        File noiseFile = new File(wip, fileName);
+
+        InputStream inputStream = null;
+        FileOutputStream fileOutputStream = null;
+
+        try {
+            inputStream = getClass().getClassLoader()
+                    .getResourceAsStream(resourceName);
+
+            byte[] buffer = new byte[inputStream.available()];
+            inputStream.read(buffer);
+
+            fileOutputStream = new FileOutputStream(noiseFile);
+            fileOutputStream.write(buffer);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+
+            try {
+                inputStream.close();
+                fileOutputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return noiseFile;
     }
 }
