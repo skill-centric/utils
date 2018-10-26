e736a4a516ef53a8c80cf0bae31441b2ae3e28fa
zavanton123 <zavanton123@gmail.com>
2018-10-26 22:09:13 +0300

    File is rendered

--- src/main/java/com/zavanton123/model/video/VideoRenderer.kt
+++ src/main/java/com/zavanton123/model/video/VideoRenderer.kt
@@ -1,11 +1,66 @@
 package com.zavanton123.model.video
 
+import com.zavanton123.model.general.TerminalCommandRunner
 import java.io.File
 
 class VideoRenderer {
 
     fun render(kdenliveFile: File) {
 
-        // todo
+        val videoName = kdenliveFile.name
+                .substringBefore(" - kdenlive.kdenlive")
+
+        val videoFileName = "$videoName.mp4"
+        println("videoFileName: $videoFileName")
+
+        val target = File(kdenliveFile.parent, videoFileName)
+
+        val kdenliveFilePath = "\"${kdenliveFile.absolutePath}\""
+        println("kdenliveFilePath: $kdenliveFilePath")
+
+        val targetFilePath = "avformat:\"${target.absolutePath}\""
+        println("targetFilePath: $targetFilePath")
+
+//        val commands = arrayOf("melt",
+//                kdenliveFilePath,
+//                targetFilePath,
+//                "-consumer",
+//                targetFilePath,
+//                "vcodec=libx264",
+//                "b=5000k",
+//                "acodec=aac",
+//                "ab=128k"
+//                )
+
+//        val commands = arrayOf("melt", kdenliveFilePath,
+//                "-consumer", "/home/zavanton/great.mp4")
+
+        val commandsOne = arrayOf("chmod", "+x", "/home/zavanton/Desktop/script.sh")
+
+        val commandsTwo = arrayOf("/home/zavanton/Desktop/script.sh")
+
+        printCommand(commandsOne)
+
+        val runner = TerminalCommandRunner()
+        val callback = object : TerminalCommandRunner.Callback {
+            override fun onSuccess() {
+                println("Success!")
+            }
+
+            override fun onFailure() {
+                println("Failed")
+            }
+        }
+
+//        runner.runCommand(commandsOne, callback)
+        runner.runCommand(commandsTwo, callback)
+    }
+
+    private fun printCommand(commands: Array<String>) {
+        val builder = StringBuilder()
+        for (command in commands) {
+            builder.append("$command ")
+        }
+        println(builder.toString() + "\n")
     }
 }
