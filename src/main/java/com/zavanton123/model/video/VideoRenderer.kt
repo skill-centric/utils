794986806a1f3acb42d5e2f88e82fbab649dd663
zavanton123 <zavanton123@gmail.com>
2018-10-27 00:14:16 +0300

    VideoRenderer is refactored into smaller methods

--- src/main/java/com/zavanton123/model/video/VideoRenderer.kt
+++ src/main/java/com/zavanton123/model/video/VideoRenderer.kt
@@ -10,35 +10,45 @@ class VideoRenderer {
 
     fun render(kdenliveFile: File) {
 
-        val videoFileName = getVideoFileName(kdenliveFile,
-                "mp4", " - kdenlive.kdenlive")
-
-        val videoFile = File(kdenliveFile.parent, videoFileName)
-
-        val kdenliveFilePath = "\"${kdenliveFile.absolutePath}\""
-        log.info("kdenliveFilePath: $kdenliveFilePath")
-
-        val videoFilePath = "\"${videoFile.absolutePath}\""
-        log.info("videoFilePath: $videoFilePath")
-
-        val command = "melt $kdenliveFilePath " +
-                "-consumer avformat:$videoFilePath " +
-                "vcodec=libx264 b=5000k acodec=aac ab=128k"
-
-        log.info("command: $command")
+        val command = setupCommand(kdenliveFile)
 
         val runner = TerminalCommandRunner()
 
         runner.runCommand(command, object : TerminalCommandRunner.Callback {
             override fun onSuccess() {
                 log.info("Success!")
-
             }
 
             override fun onFailure() {
                 log.info("Failed")
             }
         })
+    }
+
+    private fun setupCommand(kdenliveFile: File): String {
+
+        val kdenliveFilePath = "\"${kdenliveFile.absolutePath}\""
+        log.info("kdenliveFilePath: $kdenliveFilePath")
+
+        val videoFilePath = getVideoFilePath(kdenliveFile)
+        log.info("videoFilePath: $videoFilePath")
+
+        val command = "melt $kdenliveFilePath " +
+                "-consumer avformat:$videoFilePath " +
+                "vcodec=libx264 b=5000k acodec=aac ab=128k"
+        log.info("command: $command")
+
+        return command
+    }
+
+    private fun getVideoFilePath(kdenliveFile: File): String {
+
+        val videoFileName = getVideoFileName(kdenliveFile,
+                "mp4", " - kdenlive.kdenlive")
+
+        val videoFile = File(kdenliveFile.parent, videoFileName)
+
+        return "\"${videoFile.absolutePath}\""
     }
 
     private fun getVideoFileName(kdenliveFile: File,
